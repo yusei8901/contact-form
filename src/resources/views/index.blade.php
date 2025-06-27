7dfdce34485c50e6326b97b9bdda1f586c08d6c8
oshita yusei <yuseioshita061989@gmail.com>
2025-06-27 17:45:46 +0900

    20250627,17:45コミット

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,84 +1,78 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Form</title>
-    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
-</head>
-<body>
-    <header class="header">
-        <div class="header__inner">
-            <a href="/" class="header__logo">
-                Contact Form
-            </a>
-        </div>
-    </header>
+@extends('layouts.app')
 
-    <main>
-        <div class="contact-form__contact">
-            <div class="contact-form__heading">
-                <h2>お問い合わせ</h2>
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+
+@section('content')
+<div class="contact-form__contact">
+    <div class="contact-form__heading">
+        <h2>お問い合わせ</h2>
+    </div>
+    <form class="form" action="/contacts/confirm" method="post">
+        @csrf
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お名前</span>
+                <span class="form__label--required">必須</span>
             </div>
-            <form action="" class="form">
-                <div class="form__group">
-                    <div class="form__group-title">
-                        <span class="form__label--item">お名前</span>
-                        <span class="form__label--required">必須</span>
-                    </div>
-                    <div class="form__group-content">
-                        <div class="form__input--text">
-                            <input type="text" name="name" placeholder="テスト太郎">
-                        </div>
-                        <div class="form__error">
-                            <!-- パリデーション機能を実装したら記述します。 -->
-                        </div>
-                    </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="name" placeholder="テスト太郎" value="{{ old('name') }}">
                 </div>
-                <div class="form__group">
-                    <div class="form__group-title">
-                        <span class="form__label--item">メールアドレス</span>
-                        <span class="form__label--required">必須</span>
-                    </div>
-                    <div class="form__group-content">
-                        <div class="form__input--text">
-                            <input type="email" name="email" placeholder="test@example.com">
-                        </div>
-                        <div class="form__error">
-                            <!-- パリデーション機能を実装したら記述します。 -->
-                        </div>
-                    </div>
+                <div class="form__error">
+                    @error('name')
+                    {{ $message }}
+                    @enderror
                 </div>
-                <div class="form__group">
-                    <div class="form__group-title">
-                        <span class="form__label--item">電話番号</span>
-                        <span class="form__label--required">必須</span>
-                    </div>
-                    <div class="form__group-content">
-                        <div class="form__input--text">
-                            <input type="tel" name="tel" placeholder="09012345678">
-                        </div>
-                        <div class="form__error">
-                            <!-- パリデーション機能を実装したら記述します。 -->
-                        </div>
-                    </div>
-                </div>
-                <div class="form__group">
-                    <div class="form__group-title">
-                        <span class="form__label--item">お問い合わせ内容</span>
-                    </div>
-                    <div class="form__group-content">
-                        <div class="form__input--textarea">
-                            <textarea name="content" placeholder="資料をいただきたいです"></textarea>
-                        </div>
-                    </div>
-                </div>
-                <div class="form__button">
-                    <button class="form__button-submit" type="submit">送信</button>
-                </div>
-            </form>
+            </div>
         </div>
-    </main>
-</body>
-</html>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+                <span class="form__label--required">必須</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}">
+                </div>
+                <div class="form__error">
+                    @error('email')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">電話番号</span>
+                <span class="form__label--required">必須</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="tel" name="tel" placeholder="09012345678" value="{{ old('tel') }}">
+                </div>
+                <div class="form__error">
+                    @error('tel')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お問い合わせ内容</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--textarea">
+                    <textarea name="content" placeholder="資料をいただきたいです"></textarea>
+                </div>
+            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">送信</button>
+        </div>
+    </form>
+</div>
+@endsection
