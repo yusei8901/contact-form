dfd417c3253112b6b14c9a9719fc98c511549542
oshita yusei <yuseioshita061989@gmail.com>
2025-06-27 14:51:16 +0900

    20250627,14:50コミット

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+</head>
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a href="/" class="header__logo">
+                Contact Form
+            </a>
+        </div>
+    </header>
+
+    <main>
+        <div class="thanks__content">
+            <div class="thanks__heading">
+                <h2>お問い合わせありがとうございます</h2>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
